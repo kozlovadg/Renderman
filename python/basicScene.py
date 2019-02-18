3dccbb3344c1df429e5b0f959aa4300e446952ea
Daria Kozlova <daria@MacBook-Pro-Daria.local>
2019-02-18 13:28:15 +0300

    Glass shader test

--- python/basicScene.py
+++ python/basicScene.py
@@ -11,13 +11,13 @@ def main(filename,
         integrator='PxrPathTracer',
         integratorParams={}
         ) :
-  #print ('shading rate {} pivel variance {} using {} {}'.format(shadingrate,pixelvar,integrator,integratorParams))
+  print ('shading rate {} pivel variance {} using {} {}'.format(shadingrate,pixelvar,integrator,integratorParams))
     
   ri = prman.Ri() # create an instance of the RenderMan interface
 
   ri.Begin(filename)
 
-  ri.Option('searchpath', {'string archive':'./assets/:@'})
+  ri.Option('searchpath', {'string archive':'../assets/:@'})
   ri.Option('searchpath', {'string texture':'../hdri/:@'})
 
   ri.Display('watches.exr', 'it', 'rgba')
@@ -60,25 +60,25 @@ def main(filename,
   ###
   ri.AttributeBegin()
   ri.Attribute( 'user' , {'string __materialid' : ['metal'] })
-  #ri.Attribute( 'sides', {'int doubleshaded' : [1] })
-  """ri.Bxdf ('PxrSurface' , 'metal', 
-  {
-      'float diffuseGain' : [0.8],
-      'int specularFresnelMode' : [1], 
-      'color specularFaceColor' : [1,1,1], 
-      'color specularEdgeColor' : [1,1,1],
-      'color roughSpecularIor' : [2.5,2.5,2.5],
-      'float specularRoughness' : [0.01], 
-      'string __materialid' : ['metal']
-  })"""
-  ri.Bxdf('PxrDisney','metal',
+  ri.Attribute( 'Ri', {'int Sides' : [2] })
+  ri.Bxdf('PxrSurface', 'metal', {
+          'float diffuseGain' : [0],
+          'int specularFresnelMode' : [1],
+          'color specularEdgeColor' : [1 ,1 ,1],
+          'color specularIor' : [4.3696842, 2.916713, 1.654698],
+          'color specularExtinctionCoeff' : [5.20643, 4.2313662, 3.7549689],
+          'float specularRoughness' : [0.1], 
+          'integer specularModelType' : [1] ,
+          'string __materialid' : ['metal']
+  })
+  """ri.Bxdf('PxrDisney','metal',
   {
     'color baseColor' : [.25,.25,.25], 
     'float metallic' : [1], 
     'float specular' : [1], 
     'float roughness' : [0.2], 
     'string __materialid' : ['metal']
-  })
+  })"""
   ri.TransformBegin()
   widthBig = 1.25
   widthSmall = 1.1
@@ -94,20 +94,27 @@ def main(filename,
     'int maxspeculardepth' : [8]
   })
   ri.Bxdf('PxrSurface', 'glass',{ 
-  'float refractionGain' : [1.0],
-  'float reflectionGain' : [1.0],
-  'float glassRoughness' : [0.01],
-  'float glassIor' : [1.5],
+    'float diffuseGain' : [0],
+    'float refractionGain' : [1.0],
+    'float reflectionGain' : [1.0],
+    'float glassRoughness' : [0.01],
+    'float glassIor' : [1.5],
   })
   ri.Disk(-0.125,1.05,360)
   ri.Hyperboloid([ 1.05,0.0,-0.075],[1.05,0.0,-0.125],360)
+  ri.TransformBegin()
+  ri.Translate(0,0,-0.2)
+  ri.Rotate(-90,1,0,0)
+  ri.Scale(0.1,0.1,0.1)
+  ri.ReadArchive('buddha.rib')
+  ri.TransformEnd()
   ri.AttributeEnd()
   # ------------- Metal -------------
   ri.Hyperboloid([ 1.05,0.0,-0.075],[1.1,0.0,-0.1],360)
   ri.Hyperboloid([widthSmall,0.0,-0.1], [widthBig,0.0,0.0],360)
   ri.Translate(0.0, 0.0, hight/2)
   ri.Hyperboloid([widthBig,0.0,-hight/2], [widthBig,0.0,hight/2],360)
-  ri.Disk(-hight/2,widthBig,360)
+  #ri.Disk(-hight/2,widthBig,360)
   ri.TransformEnd()
   ri.AttributeEnd()
   ###
