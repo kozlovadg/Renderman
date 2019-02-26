15d86fda1eb1aa23ec97cb4520cdf0f99cd2be81
Daria Kozlova <daria@MacBook-Pro-Daria.local>
2019-02-26 10:09:33 +0300

    Main Mechanism

--- python/basicScene.py
+++ python/basicScene.py
@@ -2,7 +2,136 @@ from __future__ import print_function
 import prman
 import ProcessCommandLine as cl
 
-def detail(ri):
+def sideDetailsNearMechanism(ri):
+      # Detail 1
+      ri.TransformBegin()
+      ri.Rotate(-5, 0,0,1)
+
+      face=[0,1.325,-0.175,
+            0.15,1.24,-0.22,
+            0,1.325,-0.175,
+            0,1.25,-0.225,
+            ]	
+      ri.Patch("bilinear",{'P':face})
+
+      face=[0.05,1.5,-0.05,
+            0,1.325,-0.175,
+            0.05,1.5,0.085,
+            0,1.35,0.125]	
+      ri.Patch("bilinear",{'P':face})
+
+      face=[0.15,1.5,-0.05,
+            0.15,1.24,-0.22,
+            0.175,1.5,0.085,
+            0.15,1.22,0.125]
+      ri.Patch("bilinear",{'P':face})
+
+      face=[0.175,1.5,-0.05,
+            0.15,1.24,-0.22,
+            0.05,1.5,-0.05,
+            0,1.325,-0.175]
+      ri.Patch("bilinear",{'P':face})
+
+      face=[0.175,1.5,0.085,
+            0.15,1.22,0.125,
+            0.05,1.5,0.085,
+            0,1.35,0.125]
+      ri.Patch("bilinear",{'P':face})
+
+      face=[0.175,1.5,-0.05,
+            0.175,1.5,0.085,
+            0.05,1.5,-0.05,
+            0.05,1.5,0.085]
+      ri.Patch("bilinear",{'P':face})
+
+      ri.TransformEnd()
+
+      # Detail 2
+      ri.TransformBegin()
+      ri.Rotate(-35, 0,0,1)
+
+      face=[0,1.325,-0.175,
+            -0.15,1.24,-0.22,
+            0,1.325,-0.175,
+            0,1.25,-0.225,
+            ]	
+      ri.Patch("bilinear",{'P':face})
+
+      face=[-0.05,1.5,-0.05,
+            0,1.325,-0.175,
+            -0.05,1.5,0.085,
+            0,1.35,0.125]	
+      ri.Patch("bilinear",{'P':face})
+
+      face=[-0.175,1.5,-0.05,
+            -0.15,1.24,-0.22,
+            -0.175,1.5,0.085,
+            -0.15,1.22,0.125]
+      ri.Patch("bilinear",{'P':face})
+      
+      face=[-0.175,1.5,-0.05,
+            -0.15,1.24,-0.22,
+            -0.05,1.5,-0.05,
+            0,1.325,-0.175]
+      ri.Patch("bilinear",{'P':face})
+
+      face=[-0.175,1.5,0.085,
+            -0.15,1.22,0.125,
+            -0.05,1.5,0.085,
+            0,1.35,0.125]
+      ri.Patch("bilinear",{'P':face})
+
+      face=[-0.175,1.5,-0.05,
+            -0.175,1.5,0.085,
+            -0.05,1.5,-0.05,
+            -0.05,1.5,0.085]
+      ri.Patch("bilinear",{'P':face})
+
+      ri.TransformEnd()
+
+def detailAroundMechanism(ri):
+      index = 0
+      while index < 2:
+            ri.TransformBegin()
+            ri.Translate(0,0,-0.1*index)
+            angle = 0
+            while angle<360:
+                  
+                  ri.TransformBegin()
+                  ri.Rotate(angle-5,0,0,1)
+                  face=[0,-0.175,-1.25,
+                        0,-0.1,-1.25,
+                        0,-0.175,-1.325,
+                        0,-0.1,-1.325]	
+                  ri.Patch("bilinear",{'P':face})
+                  ri.TransformEnd()
+
+                  ri.TransformBegin()
+                  ri.Rotate(angle,0,0,1)
+                  face=[0,-0.175,-1.25,
+                        0,-0.1,-1.25,
+                        0,-0.175,-1.325,
+                        0,-0.1,-1.325]	
+                  ri.Patch("bilinear",{'P':face})
+
+                  face=[0,-0.175,-1.25,
+                        0,-0.175,-1.325,
+                        0.075,-0.16,-1.25,
+                        0.075,-0.16,-1.325]	
+                  ri.Patch("bilinear",{'P':face})
+
+                  face=[0,-0.175,-1.325,
+                        0.075,-0.16,-1.325,
+                        0,-0.1,-1.325,
+                        0.05,-0.1,-1.325]	
+                  ri.Patch("bilinear",{'P':face})
+                  ri.TransformEnd()
+
+                  angle += 30
+            ri.TransformEnd()
+            index += 1
+
+def fastenerDetail(ri):
   # Detail 3
   ri.TransformBegin()
 
@@ -157,9 +286,9 @@ def main(filename,
 
   ri.Projection(ri.PERSPECTIVE,{ri.FOV:fov})
 
-  ri.Translate(0,0.25,4)
-  ri.Rotate(-20,1,0,0)
-  ri.Rotate(-210,0,1,0)
+  ri.Translate(0,0.25,3.2)
+  ri.Rotate(-30,1,0,0)
+  ri.Rotate(-230,0,1,0)
 
   #######################################################################
   #World Begin
@@ -228,16 +357,13 @@ def main(filename,
 
   ri.Translate(0,0,0.225)
 
-  w = 0.9
-  d = 0.15
-  h = 0.03
-  face=[-w,-h+0.02,-d,
-        -w,h-0.02,-d,
-        0.15,-h,-d,
-        0.15,h,-d]
   ri.TransformBegin()
   ri.Rotate(25, 0,0,1)
-  ri.Disk(-0.15,-0.07,360)
+  ri.Disk(-0.15,0.07,360)
+  face=[-0.9,-0.01,-0.15,
+        -0.9,0.01,-0.15,
+        0.15,-0.03,-0.15,
+        0.15,0.03,-0.15]
   ri.Patch("bilinear",{'P':face})
   ri.TransformEnd()
 
@@ -246,26 +372,22 @@ def main(filename,
           'color diffuseColor' : [0.001147,0.06711,0.516743]
   })
 
-  w = 0.9
-  d = 0.125
-  h = 0.03
-  face=[-w,-h,-d,
-        -w,h,-d,
-        0.15,-h,-d,
-        0.15,h,-d]
-  ri.Disk(-0.125,-0.07,360)
+  ri.Disk(-0.155,0.0125,360)
+
+  face=[-0.9,-0.03,-0.125,
+        -0.9,0.03,-0.125,
+        0.15,-0.03,-0.125,
+        0.15,0.03,-0.125]
+  ri.Disk(-0.125,0.07,360)
   ri.Patch("bilinear",{'P':face})
 
-  w = 0.65
-  d = 0.1
-  h = 0.03
-  face=[-w,-h,-d,
-        -w,h,-d,
-        0.15,-h,-d,
-        0.15,h,-d]
   ri.TransformBegin()
   ri.Rotate(-120, 0,0,1)
-  ri.Disk(-0.1,-0.07,360)
+  ri.Disk(-0.1,0.07,360)
+  face=[-0.65,-0.03,-0.1,
+        -0.65,0.03,-0.1,
+        0.15,-0.03,-0.1,
+        0.15,0.03,-0.1]
   ri.Patch("bilinear",{'P':face})
   ri.TransformEnd()
   ri.AttributeEnd()
@@ -309,17 +431,6 @@ def main(filename,
   ri.AttributeBegin()
   ri.Attribute( 'user' , {'string __materialid' : ['metal'] })
   ri.Attribute( 'Ri', {'int Sides' : [2] })
-  """
-  ri.Bxdf('PxrSurface', 'metal', {
-          'float diffuseGain' : [0],
-          'int specularFresnelMode' : [1],
-          'color specularEdgeColor' : [1 ,1 ,1],
-          'color specularIor' : [4.3696842, 2.916713, 1.654698],
-          'color specularExtinctionCoeff' : [5.20643, 4.2313662, 3.7549689],
-          'float specularRoughness' : [0.1], 
-          'integer specularModelType' : [1] ,
-          'string __materialid' : ['metal']
-  })"""
   ri.Bxdf('PxrDisney','metal',
   {
     'color baseColor' : [.25,.25,.25], 
@@ -335,104 +446,26 @@ def main(filename,
   ri.Hyperboloid([ widthBig,0.0,-0.2],[widthBig + 0.05,0.0,-0.2],360)
   ri.Hyperboloid([ widthBig + 0.05,0.0,-0.2],[widthBig + 0.05,0.0,-0.225],360)
 
-  # Detail 1
-  ri.TransformBegin()
-  ri.Rotate(-5, 0,0,1)
-
-  face=[0,1.325,-0.175,
-        0.15,1.24,-0.22,
-        0,1.325,-0.175,
-        0,1.25,-0.225,
-        ]	
-  ri.Patch("bilinear",{'P':face})
-
-  face=[0.05,1.5,-0.05,
-        0,1.325,-0.175,
-        0.05,1.5,0.085,
-        0,1.35,0.125]	
-  ri.Patch("bilinear",{'P':face})
-
-  face=[0.15,1.5,-0.05,
-        0.15,1.24,-0.22,
-        0.175,1.5,0.085,
-        0.15,1.22,0.125]
-  ri.Patch("bilinear",{'P':face})
-
-  face=[0.175,1.5,-0.05,
-        0.15,1.24,-0.22,
-        0.05,1.5,-0.05,
-        0,1.325,-0.175]
-  ri.Patch("bilinear",{'P':face})
-
-  face=[0.175,1.5,0.085,
-        0.15,1.22,0.125,
-        0.05,1.5,0.085,
-        0,1.35,0.125]
-  ri.Patch("bilinear",{'P':face})
-
-  face=[0.175,1.5,-0.05,
-        0.175,1.5,0.085,
-        0.05,1.5,-0.05,
-        0.05,1.5,0.085]
-  ri.Patch("bilinear",{'P':face})
-
-  ri.TransformEnd()
-
-  # Detail 2
-  ri.TransformBegin()
-  ri.Rotate(-35, 0,0,1)
-
-  face=[0,1.325,-0.175,
-        -0.15,1.24,-0.22,
-        0,1.325,-0.175,
-        0,1.25,-0.225,
-        ]	
-  ri.Patch("bilinear",{'P':face})
-
-  face=[-0.05,1.5,-0.05,
-        0,1.325,-0.175,
-        -0.05,1.5,0.085,
-        0,1.35,0.125]	
-  ri.Patch("bilinear",{'P':face})
-
-  face=[-0.175,1.5,-0.05,
-        -0.15,1.24,-0.22,
-        -0.175,1.5,0.085,
-        -0.15,1.22,0.125]
-  ri.Patch("bilinear",{'P':face})
-  
-  face=[-0.175,1.5,-0.05,
-        -0.15,1.24,-0.22,
-        -0.05,1.5,-0.05,
-        0,1.325,-0.175]
-  ri.Patch("bilinear",{'P':face})
-
-  face=[-0.175,1.5,0.085,
-        -0.15,1.22,0.125,
-        -0.05,1.5,0.085,
-        0,1.35,0.125]
-  ri.Patch("bilinear",{'P':face})
-
-  face=[-0.175,1.5,-0.05,
-        -0.175,1.5,0.085,
-        -0.05,1.5,-0.05,
-        -0.05,1.5,0.085]
-  ri.Patch("bilinear",{'P':face})
-
-  ri.TransformEnd()
+  # Side details near mechanism
+  sideDetailsNearMechanism(ri)
 
   ri.TransformBegin()
   ri.Rotate(-20, 0,0,1)
   ri.Rotate(90, 1,0,0)
   ri.Translate(0,-0.025,0)
   ri.Cylinder(0.15,-1.45,-1.1,360)
+  ri.Hyperboloid([0.15,0.0,-1.45],[0.125,0.0,-1.45],360)
   
   ri.AttributeBegin()
   ri.Bxdf('PxrDiffuse', 'smooth', { 
           'color diffuseColor' : [0.001147,0.06711,0.516743]
   }) 
-  ri.Disk(-1.45,0.15,360)
+  ri.Disk(-1.45,0.125,360)
   ri.AttributeEnd()
+
+  
+  # Small details around mechanism
+  detailAroundMechanism(ri)
   
   ri.TransformEnd()
 
@@ -452,17 +485,43 @@ def main(filename,
   # Detail 3 and Detail 4
   ri.TransformBegin()
   ri.Rotate(-20, 0,0,1)
-  detail(ri)
+  fastenerDetail(ri)
   ri.TransformEnd()
 
   # Detail 5 and Detail 6
   ri.TransformBegin()
   ri.Rotate(-200, 0,0,1)
-  detail(ri)
+  fastenerDetail(ri)
   ri.TransformEnd()
 
   ri.AttributeEnd()
   ri.TransformEnd()
+
+  ri.AttributeBegin()
+  ri.Attribute( 'user' , {'string __materialid' : ['metal'] })
+  ri.Attribute( 'Ri', {'int Sides' : [2] })
+  ri.Bxdf('PxrDisney','metal',
+  {
+    'color baseColor' : [.25,.25,.25], 
+    'float metallic' : [1], 
+    'float specular' : [1], 
+    'float roughness' : [0.2], 
+    'string __materialid' : ['metal']
+  })
+  # Cylinder between details
+  ri.TransformBegin()
+  ri.Translate(1.55,-0.3,0)
+  ri.Rotate(15, 0,1,0)
+  ri.Cylinder(0.04,-1.05,0.25,360)
+  ri.TransformEnd()
+
+  ri.TransformBegin()
+  ri.Translate(-1.55,-0.3,0)
+  ri.Rotate(15, 0,1,0)
+  ri.Cylinder(0.04,-0.25,1.05,360)
+  ri.TransformEnd()
+
+  ri.AttributeEnd()
 
 
   ri.WorldEnd()
